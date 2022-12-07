703a18d08f4f84eaf12a59a6dee39ab3dc2f5664
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2022-12-07 19:46:38 +0700

    study module

---
+++ docs/assets/filename.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const changeName = function (name) {
+  name = name.replaceAll(/[-&,<>:"\/\\|?*]+/g, "");
+  name = name.replaceAll(/\s+/g, "_");
+
+  return name;
+};
+
+function writeFile(fileName, jsonData) {
+  fs.writeFileSync(fileName, JSON.stringify(jsonData));
+}
+
+let files = [];
+
+fs.readdirSync('./exams/').forEach((file) => {
+  files.push({type: 'exams', name: file.split('.')[0]});
+});
+fs.readdirSync('./lessons/').forEach((file) => {
+  files.push({type: 'lessons', name: file.split('.')[0]});
+});
+
+files.forEach((fileData) => {
+  let fileLocation = `./${fileData.type}/${fileData.name}.json`;
+
+  fs.readFile(fileLocation, {encoding: 'utf-8'}, function (err, data) {
+    if (!err) {
+      let json = JSON.parse(data);
+      json.forEach((q => {
+        let answer = q.answers;
+
+        for (let k of Object.keys(q.answers)) {
+          let ans = answer[k];
+          q.answers[k] = ans.trim();
+        }
+      }));
+
+      fs.writeFileSync(fileLocation, JSON.stringify(json));
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
