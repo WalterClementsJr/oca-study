a4087800ee76008f8e4a75a3f97065351efd1e50
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2023-02-03 22:30:47 +0700

    fix next question doesn't reset states

--- src/app/component/pages/random-question/random-question.component.ts
+++ src/app/component/pages/random-question/random-question.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {ChangeDetectorRef, Component, ViewChild} from '@angular/core';
 import {Question} from "../../../entity/Question";
 import {TrainingContentService} from "../../../service/TrainingContentService";
 import {ActivatedRoute} from "@angular/router";
@@ -6,86 +6,37 @@ import {Location} from "@angular/common";
 import {QuestionFormComponent} from "../../question-form/question-form.component";
 
 @Component({
-  // standalone: true,
-  // imports: [QuestionFormComponent],
   selector: 'app-random-question',
   templateUrl: './random-question.component.html',
   styleUrls: ['./random-question.component.css']
 })
 export class RandomQuestionComponent {
-
   @ViewChild("questionComponent") _questionComponent: QuestionFormComponent | undefined;
   question: Question | undefined;
 
-  // readonly formKey = 'answer';
-  // listOfAnswers: any[] = [];
-  // answerIsCorrect: boolean | undefined;
-  // objectKeys = Object.keys;
-  // readonly questionType = QuestionType;
-  // form!: FormGroup;
-  // answer: string | undefined;
-
   constructor(
+    private changeDetectorRef: ChangeDetectorRef,
     private trainingContentService: TrainingContentService,
     private route: ActivatedRoute,
     private location: Location
   ) {
-    console.log('constructor');
-
     this.getRandomQuestion();
-    this.setup();
-  }
-
-  // ngOnInit(): void {
-  //   this.getRandomQuestion();
-  // }
-
-  private setup() {
   }
 
   getRandomQuestion() {
     this.question = this.trainingContentService.getRandomQuestion();
-    console.log(this.question);
-    // this._questionComponent?.question = this.question;
-    // this._question?.question = question;
   }
 
   back() {
     this.location.back();
   }
 
-  onSubmit() {
-    // if (!this.form?.valid) {
-    //   return false;
-    // } else {
-    //   this._questionComponent!.checkAnswer();
-    // }
-    return true;
-  }
-
   checkAnswer() {
-    // if (this.question?.type === QuestionType.SINGLE_CHOICE) {
-    //   // check radio
-    //   let value = this.form.get(this.formKey)?.value;
-    //   if (value.toUpperCase() == this.question.answer?.toUpperCase()) {
-    //     this.answerIsCorrect = true;
-    //   } else {
-    //     this.answerIsCorrect = false;
-    //     console.log("answer", this.question.answer);
-    //   }
-    // } else if (this.question?.type === QuestionType.MULTIPLE_CHOICE) {
-    //   // checkboxes
-    //   if (this.selectedOptions.join(',').toUpperCase() === this.question.answer?.toUpperCase()) {
-    //     this.answerIsCorrect = true;
-    //   } else {
-    //     this.answerIsCorrect = false;
-    //     console.log("answer", this.question.answer);
-    //   }
-    // }
+    this._questionComponent?.checkAnswer(true);
   }
 
   next() {
     this.getRandomQuestion();
-    this.setup();
+    this.changeDetectorRef.detectChanges();
   }
 }
--- src/app/component/question-form/question-form.component.ts
+++ src/app/component/question-form/question-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {TrainingContentService} from "../../service/TrainingContentService";
 import {ActivatedRoute} from "@angular/router";
 import {Location} from "@angular/common";
@@ -10,13 +10,14 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   templateUrl: './question-form.component.html',
   styleUrls: ['./question-form.component.css']
 })
-export class QuestionFormComponent implements OnInit {
+export class QuestionFormComponent implements OnInit, OnChanges {
   readonly formKey = 'answer';
+  readonly questionType = QuestionType;
+  objectKeys = Object.keys;
+
   @Input('question') question: Question | undefined;
   listOfAnswers: any[] = [];
   answerIsCorrect: boolean | undefined;
-  objectKeys = Object.keys;
-  readonly questionType = QuestionType;
   form!: FormGroup;
   answer: string | undefined;
 
@@ -26,6 +27,10 @@ export class QuestionFormComponent implements OnInit {
     private route: ActivatedRoute,
     private location: Location
   ) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.setup();
   }
 
   ngOnInit(): void {
@@ -45,8 +50,13 @@ export class QuestionFormComponent implements OnInit {
     });
   }
 
+  /**
+   * get selected answer(s)
+   */
   get selectedOptions() {
-    return this.listOfAnswers.filter(opt => opt.checked === true).map(opt => opt.key)
+    return this.listOfAnswers
+      .filter(opt => opt.checked === true)
+      .map(opt => opt.key)
   }
 
   back() {
@@ -57,31 +67,84 @@ export class QuestionFormComponent implements OnInit {
     if (!this.form?.valid) {
       return false;
     } else {
-      this.checkAnswer();
+      this.checkAnswer(false);
     }
     return true;
   }
 
-  checkAnswer() {
+  checkAnswer(showResult?: boolean) {
+    console.log("answer", this.question?.answer);
+
     if (this.question?.type === QuestionType.SINGLE_CHOICE) {
       // check radio
       let value = this.form.get(this.formKey)?.value;
-      if (value.toUpperCase() == this.question.answer?.toUpperCase()) {
-        this.answerIsCorrect = true;
-      } else {
-        this.answerIsCorrect = false;
-
-        // TODO: remove this in prod
-        console.log("answer", this.question.answer);
-      }
-    } else if (this.question?.type === QuestionType.MULTIPLE_CHOICE) {
+      this.answerIsCorrect = this.checkCorrectAnswer(value);
+    } else {
       // checkboxes with multiple correct answers
-      if (this.selectedOptions.join(',').toUpperCase() === this.question.answer?.toUpperCase()) {
-        this.answerIsCorrect = true;
-      } else {
-        this.answerIsCorrect = false;
-        console.log("answer", this.question.answer);
-      }
+      this.answerIsCorrect = this.checkCorrectAnswer(this.selectedOptions.join(','));
+    }
+
+    if (showResult) {
+      this.getColorClass();
     }
   }
+
+  checkCorrectAnswer(answer: string) {
+    return answer.toUpperCase() === this.question?.answer?.toUpperCase();
+  }
+
+  showResult() {
+    // if (this.question?.type === QuestionType.SINGLE_CHOICE) {
+    //   // check radio
+    //   let value = this.form.get(this.formKey)?.value;
+    //   if (this.checkCorrectAnswer(value)) {
+    //     this.answerIsCorrect = true;
+    //   } else {
+    //     this.answerIsCorrect = false;
+    //
+    //     // TODO: remove this in prod
+    //     console.log("answer", this.question.answer);
+    //   }
+    // } else if (this.question?.type === QuestionType.MULTIPLE_CHOICE) {
+    //   // checkboxes with multiple correct answers
+    //   if (this.checkCorrectAnswer(this.selectedOptions.join(','))) {
+    //     this.answerIsCorrect = true;
+    //   } else {
+    //     this.answerIsCorrect = false;
+    //     console.log("answer", this.question.answer);
+    //   }
+    // }
+  }
+
+  getColorClass() {
+    console.log("printing css");
+
+    if (this.form.get(this.formKey) === undefined && this.selectedOptions.length === 0) {
+      console.log("answer undefined");
+
+      let l = this.listOfAnswers.map(ans => ({...ans, isCorrect: 'normal'}));
+      console.log(l);
+      return l;
+    }
+
+    if (this.question?.type === QuestionType.SINGLE_CHOICE) {
+      return this.listOfAnswers.map(i => ({...i, isCorrect: 'normal'}));
+    } else {
+      const correctAnswers = this.question?.answer?.split(',')!;
+      const selected = this.selectedOptions.map(a => a.key);
+
+      return this.listOfAnswers.map(ans => {
+        console.log(ans);
+
+        // check if ans is selected & is in the correct answer list
+        if (selected?.some(r => correctAnswers.indexOf(r) >= 0)) {
+          ans.isCorrect = 'right-answer';
+        } else {
+          ans.isCorrect = 'wrong-answer';
+        }
+      });
+    }
+  }
+
+
 }
