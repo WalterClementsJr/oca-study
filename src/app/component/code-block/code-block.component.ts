9b13cfc5d947d19b3c0f49c38f9888cec3b398ad
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2023-04-25 21:17:36 +0700

    code block styling

---
+++ src/app/component/code-block/code-block.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-code-block',
+  templateUrl: './code-block.component.html',
+  styleUrls: ['./code-block.component.css']
+})
+export class CodeBlockComponent {
+  @Input('content') content: string | undefined;
+}
